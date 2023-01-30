1acbaa7e815a58a9ac85596892a96a482d07cc41
Aleksandr Mukhin <alxmukhin@gmail.com>
2023-01-30 12:15:12 +0300

    Added method to fill in array with elements (input from keyboard)

--- Program.cs
+++ Program.cs
@@ -7,3 +7,15 @@ int GetNumberOfElements(string message)
     int number = int.Parse(Console.ReadLine() ?? "0");
     return number;
 }
+
+// Method fills in array
+string[] FillInArray(int number)
+{
+    string[] array = new string[number];
+    for (int i = 0; i < number; i++)
+    {
+        Console.WriteLine("Введите " + (i + 1) + "-й элемент массива:");
+        array[i] = Console.ReadLine() ?? "0";
+    }
+    return array;
+}
